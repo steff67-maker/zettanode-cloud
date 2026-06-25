d51106afeb1507bab575a9b4030a58f08112692b
steff <zubarevstefan9@gmail.com>
2026-06-25 20:50:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -126,7 +126,7 @@ async def handle_everything(m: types.Message):
             photo_base64 = base64.b64encode(fb.read()).decode('utf-8')
             
             gemini_key = os.getenv("GEMINI_API_KEY")
-            url = "https://" + "generativelanguage." + "://googleapis.com" + gemini_key
+            url = "https://generativelanguage." + "://" + gemini_key
             
             payload = {
                 "contents": [{
